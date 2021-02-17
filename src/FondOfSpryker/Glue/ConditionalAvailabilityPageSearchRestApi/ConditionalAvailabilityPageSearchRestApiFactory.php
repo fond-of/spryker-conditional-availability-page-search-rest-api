e45f0e49b52a5cbfe214f18d6526df5aeba9196a
Daniel Rose <daniel-rose@gmx.de>
2021-02-17 11:58:49 +0100

    Expand period with earliest delivery date 🧩 - make mapper plugable - add earliest delivery date expander as plugin - add unit tests - move qty mapping logic to own plugin - ...

--- src/FondOfSpryker/Glue/ConditionalAvailabilityPageSearchRestApi/ConditionalAvailabilityPageSearchRestApiFactory.php
+++ src/FondOfSpryker/Glue/ConditionalAvailabilityPageSearchRestApi/ConditionalAvailabilityPageSearchRestApiFactory.php
@@ -3,10 +3,13 @@
 namespace FondOfSpryker\Glue\ConditionalAvailabilityPageSearchRestApi;
 
 use FondOfSpryker\Glue\ConditionalAvailabilityPageSearchRestApi\Dependency\Client\ConditionalAvailabilityPageSearchRestApiToConditionalAvailabilityPageSearchClientInterface;
+use FondOfSpryker\Glue\ConditionalAvailabilityPageSearchRestApi\Dependency\Service\ConditionalAvailabilityPageSearchRestApiToConditionalAvailabilityServiceInterface;
 use FondOfSpryker\Glue\ConditionalAvailabilityPageSearchRestApi\Processor\ConditionalAvailabilityPageSearch\Mapper\ConditionalAvailabilityPageSearchMapper;
 use FondOfSpryker\Glue\ConditionalAvailabilityPageSearchRestApi\Processor\ConditionalAvailabilityPageSearch\Mapper\ConditionalAvailabilityPageSearchMapperInterface;
 use FondOfSpryker\Glue\ConditionalAvailabilityPageSearchRestApi\Processor\ConditionalAvailabilityPageSearch\Reader\ConditionalAvailabilityPageSearchReader;
 use FondOfSpryker\Glue\ConditionalAvailabilityPageSearchRestApi\Processor\ConditionalAvailabilityPageSearch\Reader\ConditionalAvailabilityPageSearchReaderInterface;
+use FondOfSpryker\Glue\ConditionalAvailabilityPageSearchRestApi\Processor\EarliestDeliveryDate\Generator\EarliestDeliveryDateGenerator;
+use FondOfSpryker\Glue\ConditionalAvailabilityPageSearchRestApi\Processor\EarliestDeliveryDate\Generator\EarliestDeliveryDateGeneratorInterface;
 use Spryker\Glue\Kernel\AbstractFactory;
 
 class ConditionalAvailabilityPageSearchRestApiFactory extends AbstractFactory
@@ -38,6 +41,38 @@ class ConditionalAvailabilityPageSearchRestApiFactory extends AbstractFactory
      */
     protected function createConditionalAvailabilityPageSearchMapper(): ConditionalAvailabilityPageSearchMapperInterface
     {
-        return new ConditionalAvailabilityPageSearchMapper();
+        return new ConditionalAvailabilityPageSearchMapper(
+            $this->getRestConditionalAvailabilityPeriodMapperPlugins()
+        );
+    }
+
+    /**
+     * @return \FondOfSpryker\Glue\ConditionalAvailabilityPageSearchRestApiExtension\Dependency\Plugin\RestConditionalAvailabilityPeriodMapperPluginInterface[]
+     */
+    protected function getRestConditionalAvailabilityPeriodMapperPlugins(): array
+    {
+        return $this->getProvidedDependency(
+            ConditionalAvailabilityPageSearchRestApiDependencyProvider::PLUGIN_REST_CONDITIONAL_AVAILABILITY_PERIOD_MAPPER
+        );
+    }
+
+    /**
+     * @return \FondOfSpryker\Glue\ConditionalAvailabilityPageSearchRestApi\Dependency\Service\ConditionalAvailabilityPageSearchRestApiToConditionalAvailabilityServiceInterface
+     */
+    protected function getConditionalAvailabilityService(): ConditionalAvailabilityPageSearchRestApiToConditionalAvailabilityServiceInterface
+    {
+        return $this->getProvidedDependency(
+            ConditionalAvailabilityPageSearchRestApiDependencyProvider::SERVICE_CONDITIONAL_AVAILABILITY
+        );
+    }
+
+    /**
+     * @return \FondOfSpryker\Glue\ConditionalAvailabilityPageSearchRestApi\Processor\EarliestDeliveryDate\Generator\EarliestDeliveryDateGeneratorInterface
+     */
+    public function createEarliestDeliveryDateGenerator(): EarliestDeliveryDateGeneratorInterface
+    {
+        return new EarliestDeliveryDateGenerator(
+            $this->getConditionalAvailabilityService()
+        );
     }
 }
